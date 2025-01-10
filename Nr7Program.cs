fd316317c43fdd36eca30583d2c9a9378a76bd84
LaraKoffy <120626493+LaraKoffy@users.noreply.github.com>
2025-01-11 00:32:11 +0100

    Add files via upload

---
+++ Nr7Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Nr7
+{
+    internal class Program
+    {
+        static bool IsLeapYear(int year)
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+                
+                return true;
+            
+            return false;
+        }
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter a year to check if it is a leap year:");
+
+            if (int.TryParse(Console.ReadLine(), out int year))
+            {
+                bool isLeap = IsLeapYear(year);
+
+                
+                if (isLeap)
+                
+                {
+                    
+                    Console.WriteLine($"{year} is a leap year.");
+                }
+                else
+                {
+                    
+                    Console.WriteLine($"{year} is not a leap year.");
+                }
+            }
+        }
+    }
+}
