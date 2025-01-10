c4c5ca4c919570c764b545d392421c23dec8aa8e
LaraKoffy <120626493+LaraKoffy@users.noreply.github.com>
2025-01-11 00:26:02 +0100

    Add files via upload

---
+++ Week2.cs
@@ -0,0 +1,50 @@
+﻿namespace Nr3
+
+{
+    internal class Program
+    {
+        static void CheckNumber(int number)
+        {
+
+
+            if (number == 0)
+            {
+                Console.WriteLine("the number is 0");
+            }
+            else if (number > 0)
+            {
+                Console.WriteLine("the number is positive");
+            }
+            else
+            {
+                Console.WriteLine("the number is negative");
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert a number");
+           string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            {
+                CheckNumber(number);
+            }
+
+        }
+
+
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
