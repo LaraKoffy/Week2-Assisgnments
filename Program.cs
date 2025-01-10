a4ce8eaa7d62c3f35670e7a62714c8c448532eed
LaraKoffy <120626493+LaraKoffy@users.noreply.github.com>
2025-01-11 00:30:03 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Nr2
+
+{
+    internal class Program
+
+    {
+        static bool IsEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Enter a number: ");
+            string input = Console.ReadLine();
+
+
+
+            if (int.TryParse(input, out int number))
+            {
+
+                if (IsEven(number))
+                {
+                    Console.WriteLine("The number is even");
+                }
+
+
+                else
+                {
+                    Console.WriteLine("The number is uneven");
+                }
+
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
